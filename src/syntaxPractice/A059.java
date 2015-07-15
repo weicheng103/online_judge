089b152e82f63a9a7dd61144d342e28e9770e191
weicheng103 <weicheng103@gmail.com>
2015-07-16 00:45:07 +0800

    win7 commit

--- src/syntaxPractice/A059.java
+++ src/syntaxPractice/A059.java
@@ -7,22 +7,30 @@ public class A059 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int set = scanner.nextInt();
+		int[] set_sum = new int[set];
 		while (scanner.hasNext()) {
+
 			for (int i = 0; i < set; i++) {
-				int sum = 0;
-				for (int j = 0; j < 2; j++) {
+//				for (int j = 0; j < 2; j++) {         NOTE:bug
+					int sum = 0;
 					int a = scanner.nextInt();
 					int b = scanner.nextInt();
 					for (int k = a; k <= b; k++) {
 						for (int l = 1; l < b; l++) {
 							if (k == l * l) {
-								 System.out.println(k);
+								System.out.println(k);
 								sum += k;
 							}
 						}
+
 					}
 					System.out.println("sum=" + sum);
-				}
+					set_sum[i] = sum;
+//				}									NOTE:bug
+			}
+			
+			for (int i = 0; i < set; i++) {
+				System.out.println("case " + (i + 1) + ":" + set_sum[i]);
 			}
 		}
 
