0e314f11fdf0d3a11fc0c3eb1479b2108607f9a3
weicheng103 <weicheng103@gmail.com>
2015-07-15 22:22:39 +0800

    migrate
    to new folder online_judge

---
+++ src/syntaxPractice/A054.java
@@ -0,0 +1,24 @@
+package syntaxPractice;
+
+import java.util.Scanner;
+
+public class A054 {
+//	// a054: 電話客服中心
+//	private static String f(String str){
+//		int sum = 0;
+//		for(int i=0;i<str.length();i++){
+//			sum = sum+
+//		}
+//	}
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNext()) {
+			String str = scanner.nextLine();
+//			System.out.println(f(str));
+		}
+	}
+}
